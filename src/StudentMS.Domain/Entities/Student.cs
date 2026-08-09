c37da09fee6ad45f123b45883e6409f1a87f12fa
Suresh <neupane16suresh@gmail.com>
2026-08-09 16:49:39 +0545

    feat: add Student entity with validation rules

---
+++ src/StudentMS.Domain/Entities/Student.cs
@@ -0,0 +1,40 @@
+﻿using StudentMS.Domain.Common;
+using StudentMS.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentMS.Domain.Entities
+{
+    public class Student: BaseEntity
+    {
+        public string FirstName { get; private set; } = default!;
+        public string LastName { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+        public DateTime DateOfBirth { get; private set; }
+        private Student() { }
+        public Student(string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+                throw new DomainException("FIrst name is required.");
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new DomainException("Last name is required.");
+            if (!string.IsNullOrWhiteSpace(email)) 
+                throw new DomainException("Email is required.");
+            if (dateOfBirth == default)
+                throw new DomainException("Date of birth is required.");
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+        public void UpdateContactDetails(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email) || !email.Contains('@'))
+                throw new DomainException("A valid Email is required.");
+            Email = email;
+        }
+    }
+}
