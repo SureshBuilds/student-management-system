5192e902cbf2b3f76e1cc93a86daca83a5fc11e5
Suresh <neupane16suresh@gmail.com>
2026-08-15 21:52:49 +0545

    feat: add Student DTOs for create, read, and update

---
+++ src/StudentMS.Application/DTOs/CreateStudentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentMS.Application.DTOs
+{
+    public class CreateStudentDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+    }
+
+
+}
---
+++ src/StudentMS.Application/DTOs/StudentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentMS.Application.DTOs;
+public class StudentDto
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+}
+
