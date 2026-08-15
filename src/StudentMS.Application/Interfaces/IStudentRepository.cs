87fd901354e1c4bd3190e26edc32e1cbae4ae0ed
Suresh <neupane16suresh@gmail.com>
2026-08-15 21:52:49 +0545

    feat: add IStudentRepository interface

---
+++ src/StudentMS.Application/Interfaces/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using StudentMS.Application.DTOs;
+using StudentMS.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentMS.Application.Interfaces;
+public interface IStudentRepository
+{
+         Task<Student?> GetByIdAsync(Guid id);
+         Task<IEnumerable<Student>> GetAllAsync();
+         Task AddAsync(Student student);
+         Task UpdateAsync(Student student);
+         Task DeleteAsync(Guid id);
+}
+
