149fbf25f281b640e1fe1348ed169bcc6d848a36
Suresh <neupane16suresh@gmail.com>
2026-08-18 17:06:39 +0545

    feat: add ICourseRepository interface

---
+++ src/StudentMS.Application/Interfaces/ICourseRepository.cs
@@ -0,0 +1,17 @@
+﻿using StudentMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentMS.Application.Interfaces
+{
+    public interface ICourseRepository
+    {
+        Task<Course?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Course>> GetAllAsync();
+        Task AddAsync(Course course);
+        Task DeleteAsync(Guid id);
+        Task UpdateAsync(Course course);
+    }
+}
+
