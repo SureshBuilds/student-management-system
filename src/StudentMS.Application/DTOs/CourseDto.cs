71b203c8acd143b53a22738bd3c4326f790ed571
Suresh <neupane16suresh@gmail.com>
2026-08-18 17:06:39 +0545

    feat: add Course DTOs for create, read, and update

---
+++ src/StudentMS.Application/DTOs/CourseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentMS.Application.DTOs
+{
+    public class CourseDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Code { get;  set; } = default!;
+        public decimal Credits { get;  set; }
+    }
+}
