173b6dd8f5b7f64a2a9ccf100907505734ccda36
Suresh Neupane <neupane16suresh@gmail.com>
2026-08-19 21:25:33 +0545

    feat: add Enrollment DTOs for create and read

---
+++ src/StudentMS.Application/DTOs/EnrollmentDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentMS.Application.DTOs
+{
+    public  class EnrollmentDto
+    {
+        public Guid Id { get; set; }
+        public Guid StudentId { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public Guid CourseId { get; set; }
+        public string CourseTitle { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime EnrolledOn{ get; set; }
+
+
+    }
+}
