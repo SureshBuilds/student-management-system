41f3a97674100bb08613b692faa37793e039a5dd
Suresh Neupane <neupane16suresh@gmail.com>
2026-08-23 13:37:02 +0545

    feat: add Fluent API configurations for Student, Course, and Enrollment

---
+++ src/StudentMS.Infrastructure/Persistence/Configurations/EnrollmentConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentMS.Domain.Entities;
+
+namespace StudentMS.Infrastructure.Persistence.Configurations;
+
+public class EnrollmentConfiguration : IEntityTypeConfiguration<Enrollment>
+{
+    public void Configure(EntityTypeBuilder<Enrollment> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Status)
+            .IsRequired()
+            .HasConversion<string>()
+            .HasMaxLength(20);
+
+        builder.Property(e => e.EnrolledOn)
+            .IsRequired();
+
+        builder.HasOne(e => e.Student)
+            .WithMany()
+            .HasForeignKey(e => e.StudentId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(e => e.Course)
+            .WithMany()
+            .HasForeignKey(e => e.CourseId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(e => new { e.StudentId, e.CourseId });
+    }
+}
---
+++ src/StudentMS.Infrastructure/Persistence/Configurations/StudentConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentMS.Domain.Entities;
+
+
+namespace StudentMS.Infrastructure.Persistence.Configurations
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.FirstName).IsRequired().HasMaxLength(100);
+            builder.Property(s => s.LastName).IsRequired().HasMaxLength(100);
+            builder.Property(s => s.Email).IsRequired().HasMaxLength(200);
+            builder.HasIndex(s=>s.Email).IsUnique();
+        }
+
+
+    }
+}
