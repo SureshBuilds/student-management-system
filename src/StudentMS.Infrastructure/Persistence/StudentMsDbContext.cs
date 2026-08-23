5d82bab0aa9f4e6c686988524634705cd464d067
Suresh Neupane <neupane16suresh@gmail.com>
2026-08-23 13:36:36 +0545

    feat: add StudentMsDbContext

---
+++ src/StudentMS.Infrastructure/Persistence/StudentMsDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentMS.Infrastructure.Persistence;
+
+    public class StudentMsDbContext :DbContext
+    {
+        public StudentMsDbContext(DbContextOptions<StudentMsDbContext> options): base(options)
+        {
+        }
+        public DbSet<Student> Students=>Set<Student>();
+        public DbSet<Course> Courses=> Set<Course>();
+        public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StudentMsDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+            // Configure entity relationships and constraints here if needed
+        }
+    }
